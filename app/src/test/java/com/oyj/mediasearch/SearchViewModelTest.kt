20a1e7f5c85be35e4b32497235c57ee43769fed6
OYJ <oyj7677@gmail.com>
2025-02-26 09:24:19 +0900

    Test : 카카오 비디오 검색 테스트
    1. json 파일을 사용하여 응답 구현
    2. searchViewModel.searchVideo()테스트 진행

--- app/src/test/java/com/oyj/mediasearch/SearchViewModelTest.kt
+++ app/src/test/java/com/oyj/mediasearch/SearchViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.oyj.mediasearch
 
 import com.oyj.mediasearch.data.model.MediaImage
+import com.oyj.mediasearch.data.model.MediaVideo
 import com.oyj.mediasearch.data.repository.MediaRepository
 import com.oyj.mediasearch.data.repository.MediaRepositoryImpl
 import com.oyj.mediasearch.data.repository.remote.MediaRemoteSource
@@ -70,9 +71,68 @@ class SearchViewModelTest {
             )
         )
 
-        // Then : 미디어 데이터가 저장된다.
-        val actual = searchViewModel.mediaList.value
+        // Then : 이미지 데이터가 저장된다.
+        val actual = searchViewModel.imageList.value
         for (i in 0 until 3) {
+            assertThat(actual[i]).isEqualTo(expect[i])
+        }
+    }
+
+    @Test
+    fun `searchVideo_성공_케이스`() {
+        // Given : 사용자가 입력한 검색어를 세팅한다.
+        val keyword = "노머스"
+        searchViewModel.setQuery(keyword)
+
+        // When : 검색어를 기반으로 비디오 데이터 호출을 한다.
+        searchViewModel.searchVideo()
+
+        val expect = listOf(
+            MediaVideo(
+                thumbnail = "https://search3.kakaocdn.net/argon/138x78_80_pr/GO8K4K8FeMw",
+                dateTime = "2025-01-21T07:00:03.000+09:00",
+                mediaUrl = "http://www.youtube.com/watch?v=5bT529iPigg",
+                sources = "SBS Biz 증권",
+                title = "글로벌 엔터기업 노머스! 한파 딛고 반등 시작?",
+                playTime = 122
+            ),
+            MediaVideo(
+                thumbnail = "https://search2.kakaocdn.net/argon/138x78_80_pr/K2SoA4lyE5G",
+                dateTime = "2024-11-01T17:33:31.000+09:00",
+                mediaUrl = "http://www.youtube.com/watch?v=GBwOIGnNjfg",
+                sources = "경제적자유민족",
+                title = "노머스, 닷밀, 쓰리빌리언 청약 전 마지막 체크",
+                playTime = 287
+            ),
+            MediaVideo(
+                thumbnail = "https://search2.kakaocdn.net/argon/138x78_80_pr/L7X2nBffJdj",
+                dateTime = "2024-11-07T23:19:49.000+09:00",
+                mediaUrl = "http://www.youtube.com/watch?v=pTQSomwn5Rs",
+                sources = "빨간조끼아저씨",
+                title = "노머스..균등폭탄....헉....  균등만 95주 배정,,",
+                playTime = 200
+            ),
+            MediaVideo(
+                thumbnail = "https://search1.kakaocdn.net/argon/138x78_80_pr/KSyI0oWKnmy",
+                dateTime = "2024-11-12T15:54:17.000+09:00",
+                mediaUrl = "http://www.youtube.com/watch?v=3dylOtTEBuQ",
+                sources = "공모주린이의 투자백서",
+                title = "[공모주 상장 LIVE] 노머스 공모주 11/12일 상장일 주가흐름 LIVE",
+                playTime = 27609
+            ),
+            MediaVideo(
+                thumbnail = "https://search2.kakaocdn.net/argon/138x78_80_pr/pW9vsNa5wv",
+                dateTime = "2024-11-05T16:25:30.000+09:00",
+                mediaUrl = "http://www.youtube.com/watch?v=J0meS4_zzMg",
+                sources = "공모주린이의 투자백서",
+                title = "[공모주 경쟁률 LIVE] 노머스 & 닷밀 공모주 11/5일 2일차 실시간 경쟁률 LIVE",
+                playTime = 28638
+            )
+        )
+
+        // Then : 비디오 데이터가 저장된다.
+        val actual = searchViewModel.videoList.value
+        for (i in 0 until 5) {
             assertThat(actual[i]).isEqualTo(expect[i])
         }
     }
--- app/src/test/java/com/oyj/mediasearch/repository/remote/FakeMediaRemoteSourceImpl.kt
+++ app/src/test/java/com/oyj/mediasearch/repository/remote/FakeMediaRemoteSourceImpl.kt
@@ -2,15 +2,28 @@ package com.oyj.mediasearch.repository.remote
 
 import com.oyj.mediasearch.data.dto.image.KakaoImageDto
 import com.oyj.mediasearch.data.dto.image.toMediaImageList
+import com.oyj.mediasearch.data.dto.video.KakaoVideoDto
+import com.oyj.mediasearch.data.dto.video.toMediaVideoList
 import com.oyj.mediasearch.data.model.Media
 import com.oyj.mediasearch.data.repository.remote.MediaRemoteSource
 import kotlinx.serialization.json.Json
 import java.io.File
 
 class FakeMediaRemoteSourceImpl: MediaRemoteSource {
+    private val json = Json { ignoreUnknownKeys = true }
+
     override fun searchImage(keyword: String): List<Media> {
         val response = File("src/test/java/com/oyj/mediasearch/response/searchImage_노머스_Response.json").readText()
-        val resultKakaoImageDto = Json.decodeFromString<KakaoImageDto>(response)
+        val resultKakaoImageDto = json.decodeFromString<KakaoImageDto>(response)
         return resultKakaoImageDto.toMediaImageList()
+    }
+
+    override fun searchVideo(keyword: String): List<Media> {
+        val response = File("src/test/java/com/oyj/mediasearch/response/searchVideo_노머스_Response.json").readText()
+        val resultKakaoImageDto = json.decodeFromString<KakaoVideoDto>(response)
+        for (document in resultKakaoImageDto.documents) {
+            println(document)
+        }
+        return resultKakaoImageDto.toMediaVideoList()
     }
 }
