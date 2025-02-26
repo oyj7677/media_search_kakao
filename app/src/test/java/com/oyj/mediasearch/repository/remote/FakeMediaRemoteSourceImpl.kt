b2772ab9b336633c5269bf37f6755673698305e2
OYJ <oyj7677@gmail.com>
2025-02-26 11:56:09 +0900

    Test : 이미지, 비디오 검색 테스트 코드 개선
    1. MockWebServer를 사용하여 fake객체 제거
    2. 프로덕트 코드와 동일한 저장소 사용

--- app/src/test/java/com/oyj/mediasearch/repository/remote/FakeMediaRemoteSourceImpl.kt
+++ app/src/test/java/com/oyj/mediasearch/repository/remote/FakeMediaRemoteSourceImpl.kt
@@ -6,19 +6,20 @@ import com.oyj.mediasearch.data.dto.video.KakaoVideoDto
 import com.oyj.mediasearch.data.dto.video.toMediaVideoList
 import com.oyj.mediasearch.data.model.Media
 import com.oyj.mediasearch.data.repository.remote.MediaRemoteSource
+import com.oyj.mediasearch.network.KakaoApiService
 import kotlinx.serialization.json.Json
 import java.io.File
 
-class FakeMediaRemoteSourceImpl: MediaRemoteSource {
+class FakeMediaRemoteSourceImpl(): MediaRemoteSource {
     private val json = Json { ignoreUnknownKeys = true }
 
-    override fun searchImage(keyword: String): List<Media> {
+    override suspend  fun searchImage(keyword: String): List<Media> {
         val response = File("src/test/java/com/oyj/mediasearch/response/searchImage_노머스_Response.json").readText()
         val resultKakaoImageDto = json.decodeFromString<KakaoImageDto>(response)
         return resultKakaoImageDto.toMediaImageList()
     }
 
-    override fun searchVideo(keyword: String): List<Media> {
+    override suspend  fun searchVideo(keyword: String): List<Media> {
         val response = File("src/test/java/com/oyj/mediasearch/response/searchVideo_노머스_Response.json").readText()
         val resultKakaoImageDto = json.decodeFromString<KakaoVideoDto>(response)
         for (document in resultKakaoImageDto.documents) {
